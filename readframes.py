3e82c819d2e1a224524342e82631133979cd5ba0
rominavc99 <procrom1999@gmail.com>
2021-03-10 22:33:34 -0700

    9 de marzo practica

--- readframes.py
+++ readframes.py
@@ -1,4 +1,5 @@
 import wave
+import numpy as np
 
 
 goodmorning = wave.open('good-morningMan.wav', 'r')
@@ -6,3 +7,7 @@ goodmorning = wave.open('good-morningMan.wav', 'r')
 frames = goodmorning.readframes(-1)
 
 print(frames[:10])
+
+ondaConvertida = np.frombuffer (frames, dtype='int16')
+
+print (ondaConvertida [:10])
